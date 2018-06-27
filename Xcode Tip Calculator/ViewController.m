fcaa230f841c9ed6296dd7d6ce00bf886cec97c4
Nkenna Aniedobe <nkenna@fb.com>
2018-06-26 17:55:09 -0700

    HellO

--- Xcode Tip Calculator/ViewController.m
+++ Xcode Tip Calculator/ViewController.m
@@ -10,12 +10,18 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tipControl.alpha = 0;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +29,30 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+- (IBAction)onEdit:(id)sender {
+    NSArray *percentages = @[@(.1),@(.15),@(.22)];
+    double bill = [self.billField.text doubleValue];
+    NSLog(@"bill =%f", bill);
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex]doubleValue];
+    NSLog(@"tippercentage =%f", tipPercentage);
+    double tip = tipPercentage*bill;
+    NSLog(@"tip =%f", tip);
+    double total = bill+tip;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+    [UIView animateWithDuration:.2 animations:^{
+        self.tipControl.frame = CGRectMake(21, 278, 333, 29);
+        self.tipControl.alpha = 1;
+    }];
+    
+}
+- (IBAction)onEditStart:(id)sender {
+}
+- (IBAction)onEditEnd:(id)sender {
 }
 
 
